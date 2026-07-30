5b7cafbd461478a8c0742421af084ea91ea8201f
Sachin-840480 <sachin1712003@gmail.com>
2026-07-30 20:47:29 +0530

    Added New values file

---
+++ sql deep dive/NULL values.sql
@@ -0,0 +1,18 @@
+-- NULL Values
+
+-- For basic difference
+
+-- select 1 = 1;
+
+-- select 1 != 1;
+
+----------------------------------------------------------------------------
+
+-- For Null Values 
+
+-- select null = null;
+
+-- select null != null;
+
+-- Same thing as above but different writing style.
+-- select null <> null;
