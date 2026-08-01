0887e063513f7764768d3c29af621aa5a2aab6f6
Sachin-840480 <sachin1712003@gmail.com>
2026-08-01 16:03:15 +0530

    Added for user, database, system

--- sql deep dive/change timezone at a system for a database user.sql
+++ sql deep dive/change timezone at a system for a database user.sql
@@ -1,5 +1,20 @@
--- Change the timezone of the user on a system level.
+-- Change the timezone of the 'user' on a system level.
 
 ALTER USER postgres SET timezone = 'UTC';
 
 -- ALTER USER postgres SET timezone to 'UTC';   -- Same Same, but Different XD
+
+----------------------------------------------------------------------------
+
+-- -- Reseting the timezone back
+-- alter user postgres reset timezone;
+
+----------------------------------------------------------------------------
+
+-- -- Changing timezone for a 'specific database'.
+-- alter database <database_name> set timezone to 'UTC';
+
+----------------------------------------------------------------------------
+
+-- -- Changing timezone for the 'entire system'.
+-- alter system set timezone to '<timezone>'
