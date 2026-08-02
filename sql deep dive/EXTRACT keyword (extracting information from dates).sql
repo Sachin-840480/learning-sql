28e8d7bc3a95f5804be0af1be8b061c88d4b6e16
Sachin-840480 <sachin1712003@gmail.com>
2026-08-02 10:48:26 +0530

    Extract and date_trunc keyword updates

--- sql deep dive/EXTRACT keyword (extracting information from dates).sql
+++ sql deep dive/EXTRACT keyword (extracting information from dates).sql
@@ -2,6 +2,8 @@
 
 -- Extracting Day, Month, Year
 
+-- Extract Keyword
+
 -- select extract (day from date '2003/01/17') as day;
 -- 
 -- SELECT EXTRACT (MONTH FROM date '2003/01/17') AS MONTH;
@@ -9,6 +11,8 @@
 -- SELECT EXTRACT (year FROM date '2003/01/17') AS year;
 
 ----------------------------------------------------------------------------
+
+-- date_trunc keyword.
 
 -- Rounding date to a specific time.
 
