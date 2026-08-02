24cd61ac266271f99a19ce14008eb7cb00578996
Sachin-840480 <sachin1712003@gmail.com>
2026-08-02 16:27:24 +0530

    Learned about distinct keyword

---
+++ sql deep dive/DISTINCT keyword.sql
@@ -0,0 +1,7 @@
+-- Distinct Keyword
+
+SELECT DISTINCT salary FROM salaries;
+
+-- select DISTINCT salary, from_date from salaries;     -- for multiple rows
+
+-- we got more values in return from above query because, very few people have the same salary and from_date, i.e less duplicates are present, so more data is returned.
