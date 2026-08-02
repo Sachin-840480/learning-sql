943c3e346b8e8a157157c52fef33c77595d06338
Sachin-840480 <sachin1712003@gmail.com>
2026-08-02 20:39:01 +0530

    Learned Basic inner join

---
+++ sql deep dive/Inner Join.sql
@@ -0,0 +1,32 @@
+-- Inner Join.
+
+----------------------------------------------------------------------------
+
+-- Our Basic Inner join (wihtout join keyword)
+
+-- SELECT a.emp_no, b.salary FROM employees AS a, salaries AS b
+-- WHERE a.emp_no = b.emp_no
+-- ORDER BY a.emp_no;
+
+----------------------------------------------------------------------------
+
+-- select a.emp_no,
+-- concat(a.first_name, a.last_name) as "nmae",
+-- b.salary
+-- from employees as a inner join salaries as b on a.emp_no = b.emp_no;
+
+
+-- SELECT a.emp_no,
+-- concat(a.first_name, a.last_name) AS "nmae",
+-- b.salary
+-- FROM employees AS a INNER JOIN salaries AS b ON b.emp_no = a.emp_no;
+
+----------------------------------------------------------------------------
+
+-- Always try to do 'Order By' because the results are always unsorted.
+
+SELECT a.emp_no,
+concat(a.first_name, a.last_name) AS "nmae",
+b.salary
+FROM employees AS a INNER JOIN salaries AS b ON a.emp_no = b.emp_no
+ORDER BY a.emp_no ASC;
