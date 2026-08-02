99ad1cb21127064671e57f955d4a0ddcd5211f41
Sachin-840480 <sachin1712003@gmail.com>
2026-08-02 17:08:56 +0530

    updates in orderby with expressions

--- sql deep dive/order by (using with expressions).sql
+++ sql deep dive/order by (using with expressions).sql
@@ -2,8 +2,8 @@
 
 -- Using Expression with Order By.
 
-SELECT first_name, last_name FROM employees
-ORDER BY length(first_name);
+-- select first_name, last_name from employees
+-- order by length(first_name);
 
 SELECT first_name, last_name FROM employees
 ORDER BY length(first_name) DESC;
