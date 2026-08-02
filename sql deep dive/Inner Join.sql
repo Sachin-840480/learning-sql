79fb8dae2e1f6e64dd8d10348ad5f8e9a0e5b29a
Sachin-840480 <sachin1712003@gmail.com>
2026-08-02 20:40:58 +0530

    name changes

--- sql deep dive/Inner Join.sql
+++ sql deep dive/Inner Join.sql
@@ -11,13 +11,13 @@
 ----------------------------------------------------------------------------
 
 -- select a.emp_no,
--- concat(a.first_name, a.last_name) as "nmae",
+-- concat(a.first_name, a.last_name) as "name",
 -- b.salary
 -- from employees as a inner join salaries as b on a.emp_no = b.emp_no;
 
 
 -- SELECT a.emp_no,
--- concat(a.first_name, a.last_name) AS "nmae",
+-- concat(a.first_name, a.last_name) AS "name",
 -- b.salary
 -- FROM employees AS a INNER JOIN salaries AS b ON b.emp_no = a.emp_no;
 
@@ -26,7 +26,7 @@
 -- Always try to do 'Order By' because the results are always unsorted.
 
 SELECT a.emp_no,
-concat(a.first_name, a.last_name) AS "nmae",
+concat(a.first_name, a.last_name) AS "name",
 b.salary
 FROM employees AS a INNER JOIN salaries AS b ON a.emp_no = b.emp_no
 ORDER BY a.emp_no ASC;
