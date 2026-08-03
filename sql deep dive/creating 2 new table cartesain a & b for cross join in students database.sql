da84c560137dddffe9bb12471742e832915dcf97
Sachin-840480 <sachin1712003@gmail.com>
2026-08-03 11:52:20 +0530

    Added 2 new tables in students

---
+++ sql deep dive/creating 2 new table cartesain a & b for cross join in students database.sql
@@ -0,0 +1,11 @@
+-- Creating 2 table cartesianA & cartesianB.
+
+CREATE TABLE cartesianA (id INT);
+CREATE TABLE cartesianB (id INT);
+
+INSERT INTO cartesianA VALUES(1);
+INSERT INTO cartesianA VALUES(2);
+INSERT INTO cartesianA VALUES(3);
+
+INSERT INTO cartesianB VALUES(1);
+INSERT INTO cartesianB VALUES(2);
