23615cd72fd376fc1042b36dd26f43cf795c2c3d
Sachin-840480 <sachin1712003@gmail.com>
2026-08-03 11:47:16 +0530

    Learned About Right join

---
+++ sql deep dive/Right Join.sql
@@ -0,0 +1,11 @@
+-- Outer Joins
+
+-- Types of Outer Joins:-
+-- 1. Left Join / Left Outer Join
+-- 2. Right Join / Right Outer Join
+
+----------------------------------------------------------------------------
+
+-- 2. Right Join
+
+-- It is the same as left join but the inverse of it. It keeps the records from the right table which aren't a match.
