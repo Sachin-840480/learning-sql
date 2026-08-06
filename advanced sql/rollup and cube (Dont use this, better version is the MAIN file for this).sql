d402afc479ad8fac79fad85d4f4b919410f53c04
Sachin-840480 <sachin1712003@gmail.com>
2026-08-06 19:49:44 +0530

    basic knowledge for rollup and cube

---
+++ advanced sql/rollup and cube (Dont use this, better version is the MAIN file for this).sql
@@ -0,0 +1,74 @@
+-- Rollup 
+
+-- Why Grouping Data Becomes Harder ?
+
+SELECT  EXTRACT(YEAR FROM orderdate) AS "year",
+        EXTRACT(MONTH FROM orderdate) AS "month",
+        EXTRACT(DAY FROM orderdate) AS "day",
+        sum(ol.quantity)
+FROM orderlines AS ol
+GROUP BY
+    GROUPING SETS(
+        (EXTRACT(YEAR FROM orderdate)),
+        (
+            EXTRACT(YEAR FROM orderdate),
+            EXTRACT(MONTH FROM orderdate)
+        ),
+        (   
+            EXTRACT(YEAR FROM orderdate),
+            EXTRACT(MONTH FROM orderdate),
+            EXTRACT(DAY FROM orderdate)
+        ),
+        (    
+            EXTRACT(MONTH FROM orderdate),
+            EXTRACT(DAY FROM orderdate)
+        ),
+        (EXTRACT(MONTH FROM orderdate)),
+        (EXTRACT(DAY FROM orderdate)),
+        ()
+    )
+ORDER BY
+        EXTRACT(YEAR FROM orderdate),
+        EXTRACT(MONTH FROM orderdate),
+        EXTRACT(DAY FROM orderdate);
+
+----------------------------------------------------------------------------
+
+-- Rollup combines all this process into a simple line.
+-- It goes sequentially (ABC) -> (A)+(AB)+(ABC)
+
+SELECT  EXTRACT(YEAR FROM orderdate) AS "year",
+        EXTRACT(MONTH FROM orderdate) AS "month",
+        EXTRACT(DAY FROM orderdate) AS "day",
+        sum(ol.quantity)
+FROM orderlines AS ol
+GROUP BY
+    ROLLUP(
+        EXTRACT(YEAR FROM orderdate),
+        EXTRACT(MONTH FROM orderdate),
+        EXTRACT(DAY FROM orderdate)
+    )
+ORDER BY
+        EXTRACT(YEAR FROM orderdate),
+        EXTRACT(MONTH FROM orderdate),
+        EXTRACT(DAY FROM orderdate);
+
+
+-- cube combines all this process into a simple line.
+-- It also makes a possible combinations.
+
+SELECT  EXTRACT(YEAR FROM orderdate) AS "year",
+        EXTRACT(MONTH FROM orderdate) AS "month",
+        EXTRACT(DAY FROM orderdate) AS "day",
+        sum(ol.quantity)
+FROM orderlines AS ol
+GROUP BY
+    CUBE(
+        EXTRACT(YEAR FROM orderdate),
+        EXTRACT(MONTH FROM orderdate),
+        EXTRACT(DAY FROM orderdate)
+    )
+ORDER BY
+        EXTRACT(YEAR FROM orderdate),
+        EXTRACT(MONTH FROM orderdate),
+        EXTRACT(DAY FROM orderdate);
