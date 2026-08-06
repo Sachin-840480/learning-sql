5a5acc58025692f7699f1e2656cdae285bb12889
Sachin-840480 <sachin1712003@gmail.com>
2026-08-06 17:24:41 +0530

    added new table to understand grouping sets, rollup, cube

---
+++ advanced sql/Creating a Table to understand Grouping sets, Rollups, Cube.sql
@@ -0,0 +1,20 @@
+-- Creating a Table to understand Grouping sets, Rollups, Cube.
+
+CREATE TABLE sales_demo (
+    id INT PRIMARY KEY,
+    continent VARCHAR(30),
+    country VARCHAR(30),
+    city VARCHAR(30),
+    amount INT
+);
+
+INSERT INTO sales_demo
+VALUES
+(1,'Asia','India','Bangalore',1000),
+(2,'Asia','India','Chennai',2000),
+(3,'Asia','Japan','Tokyo',4000),
+(4,'Asia','Japan','Hiroshima',5000),
+(5,'Europe','United Kingdom','London',1000),
+(6,'Europe','United Kingdom','Manchester',2000),
+(7,'Europe','France','Paris',4000),
+(8,'Europe','France','Cannes',5000);
