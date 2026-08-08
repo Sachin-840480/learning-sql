e8bf549cb52d7db8d5c9066ff1822a6042d505f0
Sachin-840480 <sachin1712003@gmail.com>
2026-08-08 22:32:37 +0530

    excercise for window function 1/3 solved rest tomorrow, good night

---
+++ advanced sql/excercise for window functions.sql
@@ -0,0 +1,41 @@
+-- Excercise for Window Functions:-
+
+-- Database: World
+-- Table: Country
+
+
+-- 1. Show the population per continent.
+
+-- 2. To the previous query add on the ability to calculate the percentage of the world population.
+-- 
+-- Explanation:- What that means is that you will divide the population of that continent by the total population and multiply by 100 to get a percentage.
+-- 
+-- Make Sure:- Make sure you convert the population numbers to float using `population::float` otherwise you may see zero pop up
+-- 
+-- Try:- Try to use CONCAT AND ROUND to make the data look pretty
+
+
+-- Database: France
+-- Table: Regions (Join + Window function)
+
+-- 3. Count the number of towns per region.
+
+----------------------------------------------------------------------------
+
+-- Question 1:
+
+-- My Answer: Works Perfectly Fine.
+
+SELECT DISTINCT continent,
+    sum(population) OVER (
+        PARTITION BY continent
+    ) AS "Population per Continent"
+FROM country;
+
+-- ZTM Answer:
+
+SELECT
+  DISTINCT continent,
+  SUM(population) OVER w1 AS"continent population"
+FROM country 
+WINDOW w1 AS( PARTITION BY continent );
