7ba52f18198cdf5dae8650d888cdcc8d4f7cf0e3
Sachin-840480 <sachin1712003@gmail.com>
2026-08-09 20:30:40 +0530

    Learned Views in SQL Throughly. (Non-Materialized Views)

--- advanced sql/Views.sql
+++ advanced sql/Views.sql
@@ -50,4 +50,45 @@
 
 ----------------------------------------------------------------------------
 
+-- Solving the current salary problem.
+
+-- Question: Getting the recent date a employee got a salary bump.
+
+-- We tried to solve a problem in the "group by mental model.sql" and "Solving the current salary problem (from Group by mental model)" (last example).
+-- It Worked, but there are better ways to solve it.
+
+----------------------------------------------------------------------------
+
+-- Creating a View Named "last_salary_change"
+
+-- CREATE VIEW last_salary_change as 
+-- select  emp_no, max(from_date)
+-- from salaries
+-- group by emp_no
+-- order by emp_no;
+
+----------------------------------------------------------------------------
+
+-- Perfroming Operations on the created View.
+
+-- select * from last_salary_change;
+
+SELECT * FROM salaries
+JOIN last_salary_change USING(emp_no)
+WHERE from_date = max       -- max is column name XD.
+ORDER BY emp_no;
+
+----------------------------------------------------------------------------
+
+-- Joining Other Tables in the View to get a Result.
+
+-- Both give the same results.
+
+SELECT s.emp_no, d.dept_name, s.from_date, s.salary 
+FROM last_salary_change
+JOIN salaries AS s USING(emp_no)
+JOIN dept_emp AS de USING(emp_no)
+JOIN departments AS d USING(dept_no)
+WHERE max = s.from_date     -- max is column name XD.
+ORDER BY emp_no;
 
