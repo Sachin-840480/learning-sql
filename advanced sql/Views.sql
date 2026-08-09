6a465881fe2055062df0a33309762f0a9539dda9
Sachin-840480 <sachin1712003@gmail.com>
2026-08-09 19:07:30 +0530

    Learned the basics of Views

---
+++ advanced sql/Views.sql
@@ -0,0 +1,53 @@
+-- Views:
+
+-- Types of Views:
+
+-- 1. System Defined Views.
+-- 2. User Defined Views.
+
+--------------------------------------
+
+-- Types of System Defined Views:
+
+-- 1. Information SCHEMA Views.
+-- 2. CATALOG Views.
+-- 3. Dynamic Management Views.
+
+--------------------------------------
+
+-- Types of User Defined Views:
+
+-- 1. MATERIALIZED VIEWS (Indexed Views).
+-- 2. Non-MATERIALIZED Views (Regular Views).
+
+--------------------------------------
+
+-- Types of Non-Materialized Views:
+
+-- 1. SIMPLE Views.
+-- 2. Complex Views.
+-- 3. INLINE VIEWS (Sub-Queries).
+
+--------------------------------------
+
+-- Complete Map of Views:
+
+-- 1. SYSTEM Defined VIEWS:
+
+--     1. Information SCHEMA Views.
+--     2. CATALOG Views.
+--     3. Dynamic Management Views.
+
+-- 2. USER Defined VIEWS:
+
+--     1. MATERIALIZED VIEWS (Indexed VIEWS).
+
+--     2. Non-MATERIALIZED Views.
+
+--         1. SIMPLE Views.
+--         2. Complex Views.
+--         3. INLINE VIEWS (Sub-Queries).
+
+----------------------------------------------------------------------------
+
+
